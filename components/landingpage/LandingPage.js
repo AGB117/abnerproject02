f6d5f723fdb19bcc8865dd8d2f07abf1625b796c
AGB117 <95147352+AGB117@users.noreply.github.com>
2023-04-04 19:57:08 -0400

    changed color scheme
    changed color scheme and fixed oversized picture and figured the positioning, next need to position description of the restaurant on the left and the pic on the right

--- components/landingpage/LandingPage.js
+++ components/landingpage/LandingPage.js
@@ -5,10 +5,9 @@ function LandingPage() {
   return (
     <Fragment>
       <div className={classes.landing}>
-        <img
-          className={classes.landing}
-          src="https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
-        ></img>
+        <div>
+          <img src="https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"></img>
+        </div>
 
         <h1>
           The best burgers in the city! need to put this ection as a 2 column
@@ -23,7 +22,9 @@ function LandingPage() {
           dictumst quis.
         </p>
 
-        <h1>Our Menu</h1>
+        <div id="menu" className={classes.menuSection}>
+          <h1>Our Menu</h1>
+        </div>
         <div>Cards with burgers</div>
       </div>
     </Fragment>
