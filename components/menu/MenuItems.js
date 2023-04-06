251bc55cc82b43d966ae5acad039c5d3523a43cd
AGB117 <abner.gonzalez4@gmail.com>
2023-04-06 15:39:24 -0400

    added icons fixed card
    added icons and fixed card alignment issues

--- components/menu/MenuItems.js
+++ components/menu/MenuItems.js
@@ -1,3 +1,8 @@
+import {
+  FireIcon,
+  CurrencyDollarIcon,
+  ChatBubbleBottomCenterTextIcon,
+} from "@heroicons/react/24/outline";
 import classes from "./MenuItems.module.css";
 import Card from "../ui/Card";
 
@@ -8,11 +13,25 @@ function MenuItemList(props) {
         <div>
           <img className={classes.image} src={props.image}></img>
         </div>
+        <p className={classes.name}>{props.name}</p>
         <div>
-          <p>{props.name}</p>
-          <p>{props.description}</p>
-          <p>{props.price}</p>
-          <p>{props.calories}</p>
+          <div className={classes.itemLine}>
+            <ChatBubbleBottomCenterTextIcon
+              className={classes.descriptionIcon}
+            />
+            <p className={classes.description}>{props.description}</p>
+          </div>
+
+          <div className={classes.itemLine}>
+            <CurrencyDollarIcon className={classes.dollarIcon} />
+
+            <p className={classes.price}>{props.price}</p>
+          </div>
+
+          <div className={classes.itemLine}>
+            <FireIcon className={classes.caloriesIcon} />
+            <p className={classes.calories}>{props.calories}</p>
+          </div>
         </div>
       </Card>
     </li>
