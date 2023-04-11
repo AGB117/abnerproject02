56776c5ee171d3076590e12ede6385d069d486aa
AGB117 <abner.gonzalez4@gmail.com>
2023-04-11 12:59:34 -0400

    added menu item

--- pages/menu/index.js
+++ pages/menu/index.js
@@ -30,6 +30,15 @@ const DUMMY_MENU = [
     image:
       "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1299&q=80",
   },
+  {
+    id: "m3",
+    name: "Pizza Burger",
+    description: "Has marinara sauce and mozarella cheese",
+    price: 6.99,
+    calories: 599,
+    image:
+      "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1299&q=80",
+  },
 ];
 
 function Menu(props) {
