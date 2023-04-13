1a105e24de6920f3141192d414ee838a68f69e8e
AGB117 <abner.gonzalez4@gmail.com>
2023-04-13 15:59:04 -0400

    adding two way binding

--- components/menu/MenuItemList.js
+++ components/menu/MenuItemList.js
@@ -1,12 +1,27 @@
 import MenuItems from "./MenuItems";
 import classes from "./MenuItemList.module.css";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 function MenuItemList(props) {
+  //react state fro separating menu items by category
+  const [category, setCategory] = useState("burgers");
+
+  function filterBurgersHandler() {
+    setCategory((cat) => (cat = "burger"));
+    console.log(category);
+  }
+
+  function filterFriesHandler() {
+    setCategory((cat) => (cat = "fries"));
+    console.log(category);
+  }
   return (
     <Fragment>
       <div className={`${classes.menuItems}`}>
         <h1>Enjoy our menu!</h1>
+        <button onClick={filterBurgersHandler}>Burgers</button>
+        <button onClick={filterFriesHandler}>Fries</button>
+        <p style={{ fontSize: "20px" }}>{category}</p>
         <ul
           className={`${classes.container} ${classes.grid} ${classes.grid3Cols}`}
         >
@@ -19,6 +34,7 @@ function MenuItemList(props) {
               name={menuItem.name}
               image={menuItem.image}
               calories={menuItem.calories}
+              category={menuItem.category}
             />
           ))}
         </ul>
--- pages/menu/index.js
+++ pages/menu/index.js
@@ -8,6 +8,7 @@ const DUMMY_MENU = [
     description: "Has onion rings and BBQ sauce",
     price: 4.99,
     calories: 599,
+    category: "burger",
     image:
       "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1299&q=80",
   },
@@ -17,6 +18,7 @@ const DUMMY_MENU = [
     description: "Has avocado and bacon",
     price: 8.99,
     calories: 599,
+    category: "burger",
     image:
       "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1299&q=80",
   },
@@ -27,22 +29,37 @@ const DUMMY_MENU = [
     description: "Has marinara sauce and mozarella cheese",
     price: 6.99,
     calories: 599,
+    category: "burger",
     image:
       "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1299&q=80",
   },
   {
-    id: "m3",
-    name: "Pizza Burger",
-    description: "Has marinara sauce and mozarella cheese",
-    price: 6.99,
-    calories: 599,
+    id: "m4",
+    name: "Fries",
+    description: "Handcut fresh every day!!",
+    price: 3.99,
+    calories: 400,
+    category: "fries",
     image:
-      "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1299&q=80",
+      "https://images.unsplash.com/photo-1541592106381-b31e9677c0e5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
   },
 ];
 
+// const menuFilterByBurger = DUMMY_MENU.filter(
+//   (menu) => menu.category === "burger"
+// );
+// const menuFilterByFries = DUMMY_MENU.filter(
+//   (menu) => menu.category === "fries"
+// );
+
+const filteredMenu = (cat) =>
+  DUMMY_MENU.filter((items) => items.category === cat);
+
+//
+console.log(filteredMenu);
+
 function Menu(props) {
-  return <MenuItemList menuItems={DUMMY_MENU}></MenuItemList>;
+  return <MenuItemList menuItems={filteredMenu("fries")}></MenuItemList>;
 }
 
 export default Menu;
