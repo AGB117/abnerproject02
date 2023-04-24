7be9db726187e2dbccb697fc719ad06137e7be7c
AGB117 <abner.gonzalez4@gmail.com>
2023-04-24 15:18:10 -0400

    worked on form validation

--- components/contactusform/ContactUsForm.js
+++ components/contactusform/ContactUsForm.js
@@ -1,19 +1,88 @@
-import { Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import classes from "./ContactUsForm.module.css";
 import Card from "../ui/Card";
+import { useState, useRef } from "react";
 
 function ContactUsForm(props) {
+  /*forms*/
+  /*refs*/
+  const nameInputRef = useRef();
+  /*state*/
+  const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setenteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  //effect
+
+  useEffect(() => {}, [enteredNameIsValid]);
+  /*handlers*/
+  const nameInputChangeHandler = (event) => {
+    /*onChange prob produces an event in the dom ,you can accese the value of the input in event.target.value*/
+
+    setEnteredName(event.target.value);
+
+    if (enteredName.trim() !== "") {
+      setenteredNameIsValid(true);
+    }
+  };
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+    //check for empty fields, must be placed at the bregining to cancel consecuent form handler execution
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === "") {
+      setenteredNameIsValid(false);
+      return;
+    }
+
+    setenteredNameIsValid(true);
+
+    console.log(enteredName);
+
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+
+    /*reset fields*/
+    // nameInputRef.current.value="" dont manipulate the dom with vanilla js
+    setEnteredName("");
+  };
+
+  const nameInputBlurHandler = (event) => {
+    setEnteredNameTouched(true);
+    //validation
+    if (enteredName.trim() === "") {
+      setenteredNameIsValid(false);
+    }
+  };
+
+  /*export both validations into a variable*/
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
   return (
     <Fragment>
       <div className={classes.contactUsContainer}>
         <div className={classes.formContainer}>
           <Card>
-            <form className={classes.formSpacing}>
+            <form
+              className={classes.formSpacing}
+              onSubmit={formSubmissionHandler}
+            >
               <h1>Contact us</h1>
               <div className={classes.field}>
-                <label htmlFor="name">Name:</label>
+                <label htmlFor="name">First Name:</label>
                 <div>
-                  <input id="name" type="text" />
+                  <input
+                    onBlur={nameInputBlurHandler}
+                    value={enteredName}
+                    ref={nameInputRef}
+                    id="name"
+                    type="text"
+                    onChange={nameInputChangeHandler}
+                  />
+                  {nameInputIsInvalid && (
+                    <span className={classes.invalidInput}>Invalid input!</span>
+                  )}
                 </div>
               </div>
               <div className={classes.field}>
@@ -45,8 +114,8 @@ function ContactUsForm(props) {
                   <input id="comments" type="text" />
                 </div>
               </div>
-              <div className={classes.submitButton}>
-                <button>Submit</button>
+              <div className={classes.submitButtonCenter}>
+                <button className={classes.submitButton}>Submit</button>
               </div>
             </form>
           </Card>
