411a94f46de53b7f1767f55c39fe805ac8368311
AGB117 <abner.gonzalez4@gmail.com>
2023-04-26 17:01:26 -0400

    new hero section

--- components/landingpage/LandingPage.js
+++ components/landingpage/LandingPage.js
@@ -4,23 +4,11 @@ import classes from "./LandingPage.module.css";
 function LandingPage() {
   return (
     <Fragment>
-      <section id="hero">
-        <div className={`${classes.grid} ${classes.grid2cols}`}>
-          <div className={classes.hero}>
-            {/* this is the hero inside container */}
+      <section id="hero" className={classes.heroBackgroundImg}>
+        <div className={classes.heroContainer}>
+          <h1>The best burgers in the city of Orlando!</h1>
 
-            <div>
-              <h1>The best burgers in the city of Orlando!</h1>
-
-              <p>Our burgers are made with 100% angus beef, never frozen!!</p>
-            </div>
-          </div>
-          <div className={classes.imageSection}>
-            <img
-              className={classes.heroImg}
-              src="https://images.unsplash.com/photo-1571091718767-18b5b1457add?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80"
-            ></img>
-          </div>
+          <p>Our burgers are made with 100% angus beef, never frozen!!</p>
         </div>
       </section>
 
