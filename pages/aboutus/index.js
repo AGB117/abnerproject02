6da23c6498acb5cb2be3b812cc5bc9e3dcb1f243
AGB117 <abner.gonzalez4@gmail.com>
2023-04-28 14:22:45 -0400

    build error fix 2

---
+++ pages/aboutus/index.js
@@ -0,0 +1,17 @@
+import { Fragment } from "react";
+import Head from "next/head";
+// import AboutUs from "@/components/aboutus/AboutUs";
+
+function AboutUs(props) {
+  return (
+    <Fragment>
+      <Head>
+        <title>Burger Place | About us</title>
+        <link rel="icon" type="image/x-icon" href="/favicon.png" />
+      </Head>
+      {/* <LocationsList /> */}
+    </Fragment>
+  );
+}
+
+export default AboutUs;
