c31607b38ab48d40304201ac36f8709a388641a2
AGB117 <abner.gonzalez4@gmail.com>
2023-05-11 14:13:17 -0400

    fix build error pt3

--- components/Locations/LocationsList.js
+++ components/Locations/LocationsList.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import classes from "./locationsList.module.css";
+import classes from "./LocationsList.module.css";
 
 function LocationsList(props) {
   return (
