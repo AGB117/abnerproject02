9b0eff2b43853c2f994391c8901c8a9908f24a78
AGB117 <abner.gonzalez4@gmail.com>
2023-05-25 09:06:35 -0400

    fixed overflow and scrollbar
    fixed overflow-x showing on the body and made scrollbar transparent when Y scrolling on mobile

--- components/layout/MainNav.js
+++ components/layout/MainNav.js
@@ -66,32 +66,32 @@ function MainNav(props) {
   useEffect(() => {
     //this is only for the cart modal,
     if (cartModal === true) {
-      document.body.style.overflow = "hidden";
+      document.body.style.overflowY = "hidden";
     } else {
-      document.body.style.overflow = "scroll";
+      document.body.style.overflowY = "scroll";
     }
 
     if (navModal === true) {
-      document.body.style.overflow = "hidden";
+      document.body.style.overflowY = "hidden";
     } else {
-      document.body.style.overflow = "scroll";
+      document.body.style.overflowY = "scroll";
     }
 
     if (!mobileNav === true) {
-      document.body.style.overflow = "scroll";
+      document.body.style.overflowY = "scroll";
     }
 
     if (!mobileNav === true && cartModal === true) {
-      document.body.style.overflow = "hidden";
+      document.body.style.overflowY = "hidden";
     }
 
     if (cartModal === true && mobileNav === true) {
-      document.body.style.overflow = "hidden";
+      document.body.style.overflowY = "hidden";
     }
   }, [cartModal, navModal, windowWidth]);
 
   ////////////////////////////////////////
-  /////////////////cart scroll///////////
+  /////////////////cart overflow///////////
 
   ///states
   console.log(`navModal is open? ${navModal}`);
